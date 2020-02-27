e208a4ec89bb9b18f91c15b06bc71df956a13887
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-26 23:43:42 -0800

    Defined routes that will be used in the app.
    This commit breaks the app,
    because the route files don't exist in the repo yet.

--- server.js
+++ server.js
@@ -8,4 +8,9 @@ const app = express();
 
 app.get('/', (req, res) => res.json({ msg: 'Hello, from Contact Keeper!' }));
 
+// Define routes
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
 app.listen(PORT, () => console.log(`Server started listening on port ${PORT}`));
