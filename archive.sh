380c3b262e28b4f373a32aed132c640282461793
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-26 23:05:33 -0800

    Added start server

--- archive.sh
+++ archive.sh
@@ -4,4 +4,7 @@ npm init -y
 
 npm i express bcryptjs jsonwebtoken config express-validator mongoose 
 
-npm i -D nodemon concurrently 
+npm i -D nodemon concurrently
+
+# Start the server by
+npm run server
