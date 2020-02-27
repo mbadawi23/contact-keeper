c3d1479d8028062741b7120708f9ac38442e6799
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-27 08:10:45 -0800

    Converted connectDB to async/await.

--- config/db.js
+++ config/db.js
@@ -3,17 +3,18 @@ const config = require('config');
 
 const db = config.get('mongoURI');
 
-const connectDB = () => {
-  mongoose
-    .connect(db, {
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
       useNewUrlParser: true,
       useUnifiedTopology: true
-    })
-    .then(() => console.log('MongoDB connected.'))
-    .catch(err => {
-      console.log(err.message);
-      process.exit(1);
     });
+
+    console.log('MongoDB connected.');
+  } catch (error) {
+    console.log(err.message);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
