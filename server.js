4518c2343286ae37dc24dbb3536c585a2f8fdb20
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-27 08:03:16 -0800

    Importing connectDB, and connecting to database.

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
-const PORT = process.env.PORT || 7878;
-
 // the following line brings in `express`. This syntax called common js.
 // This is different from REACT which uses ES7 or something like that.
 const express = require('express');
+const connectDB = require('./config/db');
+
+const PORT = process.env.PORT || 7878;
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.json({ msg: 'Hello, from Contact Keeper!' }));
 
