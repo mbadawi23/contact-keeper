93b30d69471e62532ad7e7cc6585b5aad0ab61e0
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-02-27 08:07:38 -0800

    Avoiding warnings from mongo server.

--- config/db.js
+++ config/db.js
@@ -5,7 +5,10 @@ const db = config.get('mongoURI');
 
 const connectDB = () => {
   mongoose
-    .connect(db)
+    .connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
     .then(() => console.log('MongoDB connected.'))
     .catch(err => {
       console.log(err.message);
