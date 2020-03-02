76a30348723d31fad31bf920f5143758ed7a4abc
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-03-01 23:12:25 -0800

    Saving user to database after validation.

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
@@ -19,12 +20,40 @@ router.post(
       'Please enter a password of 4 or more charachters.'
     ).isLength({ min: 4 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
-    } else {
-      res.send('Success.');
+    }
+
+    console.log('req.body', req.body);
+    const { name, email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ msg: 'A user with this email already exists.' });
+      }
+
+      user = new User({
+        name,
+        email,
+        password
+      });
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, 10);
+
+      await user.save();
+
+      res.send('User saved.');
+    } catch (error) {
+      console.log(error);
+      res.status(500).send('Server Error.');
     }
   }
 );
