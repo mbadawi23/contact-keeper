7aa616473988987812f3b16ba2df5243ab8860d1
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-03-11 08:49:59 -0700

    Added navbar, home, and about to app.

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react';
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About Contact Keeper</h1>
+      <p className='my-1'>This is a full-stack REACT app using MERN model.</p>
+      <p className='bg-dark p'>Version: 1.0.0</p>
+    </Fragment>
+  );
+};
+
+export default About;
---
+++ client/src/components/pages/Home.js
@@ -0,0 +1,7 @@
+import React, { Fragment } from 'react';
+
+const Home = () => {
+  return <Fragment>No place like home!</Fragment>;
+};
+
+export default Home;
