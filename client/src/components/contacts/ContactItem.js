20d1f022bc79f5380ebab3f47ec38bb61fe93354
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-03-12 00:37:40 -0700

    Beefed up the contact card with data and buttons.

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 const ContactItem = ({ contact }) => {
-  const { id, name, email, phone, type, notes } = contact;
+  const { name, email, phone, type, notes } = contact;
   return (
     <div className='card bg-light'>
       <h3 className='text-primary text-left'>
@@ -15,8 +16,31 @@ const ContactItem = ({ contact }) => {
           {type.charAt(0).toUpperCase() + type.slice(1)}
         </span>
       </h3>
+      <ul className='list'>
+        {email && (
+          <li>
+            <i className='fas fa-envelope-open'> {email}</i>
+          </li>
+        )}
+        {phone && (
+          <li>
+            <i className='fas fa-phone'> {phone}</i>
+          </li>
+        )}
+        {notes && (
+          <li>
+            <i className='fas fa-file-alt'> {notes}</i>
+          </li>
+        )}
+      </ul>
+      <button className='btn btn-dark btn-sm'>Edit</button>
+      <button className='btn btn-danger btn-sm'>Delete</button>
     </div>
   );
+};
+
+ContactItem.propTypes = {
+  contact: PropTypes.object.isRequired
 };
 
 export default ContactItem;
