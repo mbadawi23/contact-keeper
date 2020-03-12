f9ae71a32195d58103c12b3fe70166f24347bd17
Mohamed Elkammar <mohamed.elkammar@gmail.com>
2020-03-12 10:43:05 -0700

    Coded reducer case for addContact.

---
+++ client/src/context/contact/contactReducer.js
@@ -0,0 +1,25 @@
+import {
+  ADD_CONTACT,
+  DELETE_CONTACT,
+  UPDATE_CONTACT,
+  SET_CURRENT,
+  CLEAR_CURRENT,
+  FILTER_CONTACTS,
+  CLEAR_FILTER,
+  SHOW_ALERT,
+  REMOVE_ALERT
+} from '../types';
+
+export default (state, action) => {
+  switch (action.type) {
+    case ADD_CONTACT:
+      return {
+        ...state,
+        // because state is immutable, we need to copy, then add.
+        contacts: [...state.contacts, action.payload]
+      };
+
+    default:
+      return state;
+  }
+};
